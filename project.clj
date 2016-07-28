0fffeb6d5dbd377338527b9db53a64181cdfa507
Alexander Kahoun <akahoun@demark.com>
2016-07-27 21:32:33 -0700

    Updated project description and url -----------------------------------------------------------------
    ```
    
     |||\    ;;;;;;;;;    /|||
     |||||\;;;;;;;;;;;;;/|||||
     |||||;; --------- ;;|||||
     |||||;;;;;;;;;;;;;;;|||||
      \|||;;;;;;;;;;;;;;;|||/
        \_;;;;;;;;;;;;;;;_/
           ;;;;;;;;;;;;;
    
    ```

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-docker "1.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen plugin that generates a Dockerfile for you project"
+  :url "https://github.com/leftofnull/lein-docker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
